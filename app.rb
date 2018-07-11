c3db814e1ff1c99be10c8f48fad7a138d044efac
Kayla Reopelle <kreopelle@gmail.com>
2018-07-11 13:57:07 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -3,8 +3,7 @@ require_relative 'config/environment'
 class App < Sinatra::Base
 
   get '/' do
-    "Welcome to your app!!!! I BUILT THIS!!!!"
-    "Muahahaha!" 
+    "Started my server using Shotgun!""
   end
 
 end
